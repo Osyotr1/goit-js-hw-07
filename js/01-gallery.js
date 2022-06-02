f4c3a2a1c3bfe8e4fe8adc5e616b9f2fb095d328
Volodymyr Horin <vova.gorin08@gmail.com>
2022-06-02 23:06:20 +0300

    1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,20 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector('.gallery');
+
+// console.log(galleryItems);
+
+const galleryEl = galleryItems.map((item) => 
+   `<div class="gallery__item"><a class="gallery__link" href=${item.original}>
+    <img
+      class="gallery__image"
+      src=${item.preview}
+      data-source=${item.original}
+      alt=${item.description}
+    />
+  </a>
+</div>`
+).join(" ");
+
+galleryList.insertAdjacentHTML('afterbegin', galleryEl);
